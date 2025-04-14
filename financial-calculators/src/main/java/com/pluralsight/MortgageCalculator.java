0774be2f3d50d78b65030b376c459f4231fe9872
Bhim Gurung <gbhimsingh@ymail.com>
2025-04-14 12:50:40 -0400

    Completed mod4 Exercise: mortgage calculator

---
+++ financial-calculators/src/main/java/com/pluralsight/MortgageCalculator.java
@@ -0,0 +1,34 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class MortgageCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the loan amount (principle): $");
+        double principle = input.nextDouble();
+
+        System.out.print("Enter the annual interest rate: ");
+        double annualRate = input.nextDouble();
+
+        System.out.print("Enter the loan amount in year: ");
+        int year = input.nextInt();
+
+        double monthlyRate = annualRate/100/12;
+        int numberOfPayments = year * 12;
+
+        double monthlyPayment = principle * (monthlyRate) /
+                (1 - Math.pow(1 + monthlyRate, -numberOfPayments));
+
+        double totalPaid = monthlyPayment * numberOfPayments;
+        double totalInterestPaid = totalPaid - principle;
+
+        System.out.printf("Your monthly payment is: $%.2f\n", monthlyPayment);
+        System.out.printf("Total interest paid on the loan: $%.2f\n", totalInterestPaid);
+
+
+
+
+
+    }
+}
